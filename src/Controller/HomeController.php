1e95a40e91e1c3f8716520320f3c818077079a31
AdrienPC <adrien_paris@yahoo.fr>
2022-03-16 18:06:37 +0100

    V10

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
+use App\Entity\Status;
+use App\Repository\ProjectRepository;
+use App\Repository\StatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +15,27 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(): Response
+    public function index(ProjectRepository $repoProject, StatusRepository $status): Response 
     {
+        $projectAll = $repoProject->findAll();
+        $aleatNumber = [1,2,3];
+
+
+        #----------------   Projets aleatoire
+        $projectbyStatusVote = $repoProject->findBy(array ('status' => '1'));
+        #----------------   Projets ouvert au vote 
+        $projectbyStatusVote = $repoProject->findBy(array ('status' => '1'));
+        #----------------   Projets ouvert au candidature 
+        $projectbyStatusCandid = $repoProject->findBy(array ('status' => '2'));
+        #----------------   Projets clos
+        $projectbyStatusFinished = $repoProject->findBy(array ('status' => '3'));
+
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'vote'=> $projectbyStatusVote,
+            'projectbyStatusVote'=>$projectbyStatusVote,
+            'repoProject'=>$repoProject
         ]);
     }
 }
