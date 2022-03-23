f9aef0fe80a5d0c89449200d79ad1fa1f1d87c14
AdrienPC <mael.aguessy@gmail.com>
2022-03-23 16:28:05 +0100

    mael save

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Project;
-use App\Entity\Status;
 use App\Repository\ProjectRepository;
-use App\Repository\StatusRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,29 +13,21 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="app_home")
      */
-    public function index(ProjectRepository $repoProject, StatusRepository $status): Response 
+    public function index(ProjectRepository $ProjectRepo): Response
     {
-        $projectAll = $repoProject->findAll();
-        $aleatNumber = [1,2,3];
-
-
-        #----------------   Projets aleatoire
-        $projectbyStatusVote = $repoProject->findBy(array ('status' => '1'));
-        #----------------   Projets ouvert au vote 
-        $projectbyStatusVote = $repoProject->findBy(array ('status' => '1'));
-        #----------------   Projets ouvert au candidature 
-        $projectbyStatusCandid = $repoProject->findBy(array ('status' => '2'));
-        #----------------   Projets clos
-        $projectbyStatusFinished = $repoProject->findBy(array ('status' => '3'));
-
+        $projects = $ProjectRepo -> findAll();
+        $projectsByStat1 = $ProjectRepo -> findBystatus(1);
+        $projectsByStat2 = $ProjectRepo -> findBystatus(2);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'vote'=> $projectbyStatusVote,
-            'projectbyStatusVote'=>$projectbyStatusVote,
-            'repoProject'=>$repoProject
+            'projectAll' => $projects,
+            'projectstatus1' => $projectsByStat1,
+            'projectstatus2' => $projectsByStat2,
+       
         ]);
     }
 
 
 }
+
--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -3,17 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Project;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Test\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Test\FormBuilderInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProjectController extends AbstractController
 {
 
     public function create(Request $request)
     {
-        $this->denyAccessUnlessGranted('ROLE_MAKER')
+        $this->denyAccessUnlessGranted('ROLE_MAKER');
 
         $project = new Project() ;
 
