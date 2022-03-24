54445ede1784bc8578ee8634228cc14403453dda
mael <maelnmc@MacBook-Pro-de-Mael.local>
2022-03-24 10:58:07 +0100

    c'est un bordel

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
-=======
 <?php
 
 namespace App\Controller;
@@ -24,20 +15,16 @@ class HomeController extends AbstractController
      */
     public function index(ProjectRepository $ProjectRepo): Response
     {
-        $projects = $ProjectRepo -> findAll();
-        $projectsByStat1 = $ProjectRepo -> findBystatus(1);
-        $projectsByStat2 = $ProjectRepo -> findBystatus(2);
+        $projects = $ProjectRepo->findAll();
+        $projectsByStat1 = $ProjectRepo->findBystatus(1);
+        $projectsByStat2 = $ProjectRepo->findBystatus(2);
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'projectAll' => $projects,
             'projectstatus1' => $projectsByStat1,
             'projectstatus2' => $projectsByStat2,
-       
+
         ]);
     }
-
-
 }
-
->>>>>>> ed9c2708c81582455f6d770aa7b4d0f3f39a5e62
